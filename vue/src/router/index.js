dd36a6c327857ac769aef0693cf2e9f7f2267d44
jamshid <jamshidqochqorov84@gmail.com>
2024-01-21 23:54:21 +0500

    added files(Register,Dashboard,Surveys) authentication front

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,15 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import Survey from '@/views/Survey.vue'
+import DefoultDashboard from '@/components/DefoultDashboard.vue'
+import store from "@/store/index.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+      {
+          path: '/',
+          redirect:'/dashboard',
+          component: DefoultDashboard,
+          children:[
+              {
+                  name:'dashboard',
+                  path:'/dashboard',
+                  component:Dashboard,
+                  meta: { requiresAuth: true },
+              },
+              {
+                  path:'/surveys',
+                  component:Survey
+              }
+
+          ]
+      },
     {
       path: '/login',
       name: 'login',
       component: Login
     },
+      {
+          path: '/register',
+          name: 'register',
+          component: Register
+      },
+
   ]
 })
-
+router.beforeEach((to, from,next)=>{
+     if(to.meta.requiresAuth && !store.state.user.token){
+         next({name:'login'})
+     }
+     else if(store.state.user.token && (to.name==='login'||to.name==='register')){
+          next({name:'dashboard'})
+     }
+     else {
+         next()
+     }
+})
 export default router
